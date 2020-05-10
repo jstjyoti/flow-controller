8768659b7bfe56fb1f7ca41e8ea4089dead0243f
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-10 14:41:51 +0530

    added store and reducers for bringing redux into our react-app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import reducer from '../src/store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(reducer);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
