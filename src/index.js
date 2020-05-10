bd6f6a4cdf73d8be17c7a940690fbd95c7adabaf
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-10 14:47:17 +0530

    connecting redux and react using Provider component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux';
+import { Provider } from 'react-redux'
 import reducer from '../src/store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer);
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
