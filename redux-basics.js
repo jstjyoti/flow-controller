3b66541a9577b2d13d694c3eb07ef9555259ac1a
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-10 13:07:29 +0530

    initiation of individual redux flow as a nodeJs file

---
+++ redux-basics.js
@@ -0,0 +1,34 @@
+// redux is independent of react. it is a stand alone library.
+const redux = require('redux');
+const createStore = redux.createStore;
+const initState = {
+    count: 0
+};
+
+//Reducer 
+const rootReducer = (state = initState, action) => {
+        if(action.type === 'INC_COUNTER'){
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        }
+        if(action.type === 'ADD_COUNTER'){
+            return {
+                ...state,
+                count: state.count + action.value
+            }
+        }   
+        
+        return state;
+    };
+
+//Store
+const store = createStore(rootReducer);
+
+//Dispatcher Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+
+//subscription
+
